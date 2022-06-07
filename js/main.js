fe514754a11223a9783a7804a31891b434ef3f50
Maxim Goncearuc <m.goncearuc344@gmail.com>
2022-06-08 00:38:21 +0200

    2.17. Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* Функция для проверки максимальной длины строки
+  Результат: true, если строка проходит по длине, и false — если не проходит */
+
+stringLength(stringCheck, stringMaxLength) {
+  stringMaxLength = (0, 140); // Результат: целое число из диапазона "от 0 до 140 cимволов"
+  if (stringLength <= stringMaxLength) {
+    return true;
+  } else {
+
+  } return false;
+
+  }
+
+  stringCheck();
+  stringLength();
+
